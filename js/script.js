f9bf90da98d2575cb8f7561ae921d49c2cd13706
krishna <yacharenikrishnakanth@gmail.com>
2023-11-16 01:28:48 -0600

    Updated with the details

--- js/script.js
+++ js/script.js
@@ -1,37 +1,33 @@
-/**
- * @author Qazi Maaz Arshad <qazimaazarshad@gmail.com>
- */
 (function ($) {
     "use strict";
-      $('.sakura-falling').sakura();
+    $('.sakura-falling').sakura();
 })(jQuery);
 
-$(document).on('click', function(){
+$(document).on('click', function () {
     document.getElementById("my_audio").play();
-    console.log('Shaadi Mein Zaroor Aana');
 });
 
 // Set the date we're counting down to
 var countDownDate = new Date("Dec 18, 2022 18:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>" + days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
@@ -71,9 +67,9 @@ var styles2 = [
     , 'font-size: 32px'
 ].join(';');
 
-console.log('\n\n%c SAVE THE DATE: 29th Nov, 2020!', styles);
+console.log('\n\n%c SAVE THE DATE: 18th Nov, 2023!', styles);
 
-console.log('%cYour presence is requested!%c\n\nRegards: Vinit Shahdeo', styles1, styles2);
+console.log('%cYour presence is requested!%c\n\nRegards: Abhinav Panjala', styles1, styles2);
 
 console.log(
     `%cShaadi me zaroor aana!\n\n`,
